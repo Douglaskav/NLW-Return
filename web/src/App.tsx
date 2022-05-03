6367da365f37548ef7d9661d5b73a15909e41b46
Douglaskav <dnmsmpvp@gmail.com>
2022-05-03 14:33:41 -0300

    Dia 1

---
+++ web/src/App.tsx
@@ -0,0 +1,5 @@
+import { Widget } from "./components/Widget.tsx";
+
+export function App() {
+  return <Widget />;
+}
---
+++ web/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.tsx", "./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257e6",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
